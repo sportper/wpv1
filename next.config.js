04fe53b3a791fc22a16b5639591e86cd020a6893
Toan Le <sportper.com@gmail.com>
2023-10-03 10:58:57 +0700

    fix anh

--- next.config.js
+++ next.config.js
@@ -2,8 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [process.env.WP_IMAGES_URL],
-  },
+    loader: 'imgix',
+    path: '',
+  }
 };
 
 module.exports = nextConfig;
